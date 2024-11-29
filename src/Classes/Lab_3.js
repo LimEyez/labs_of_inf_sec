5b0d418beac8e0bcde82147a6034112dd0077c11
LimEyez <domaales@gmail.com>
2024-11-30 01:47:40 +0400

    add RC5 algorithm

---
+++ src/Classes/Lab_3.js
@@ -0,0 +1,235 @@
+import { CustomError } from '@/Classes/CustomError.ts'
+
+export class Lab_3 {
+    constructor({ W = 32, R = 12, Key = "keyWord" } = {}) {
+        this.W = this.checkW(W);
+        this.W8 = Math.floor(this.W / 8)
+        this.W4 = Math.floor(this.W / 4)
+        this.R = this.checkR(R);
+        this.key = Key;
+        this.b = Key.length;
+        
+        if (this.b > 255) {
+            throw new Error("Длина ключа превышает 255 символов!")
+        }
+        
+        this.T = 2 * (this.R + 1);
+        this.Uint8ArrayKey = new TextEncoder().encode(this.key);
+        
+        //Константы P и Q
+        const [P, Q] = this.generateConstants();
+        this.P = P;
+        this.Q = Q;
+
+        this.mod = Math.pow(2, this.W);
+        this.mask = this.mod-1;
+        
+        this.c = null;
+        this.L = null;
+
+        this.S = [];
+
+        this.keyAlign();
+        this.extendedKeyTable();
+        this.mixing();
+
+        console.log(this)
+    }
+    // Проверка введенного W
+    checkW(W) {
+        switch (W) {
+            case 16:
+            case 32:
+            case 64:
+                return W;
+            default:
+                throw new Error(`Недопустимое значение W: ${W}. Ожидалось 16, 32 или 64.`);
+        }
+    }
+
+    // Проверка введенного R
+    checkR(R) {
+        if (R < 0 || R > 255) {
+            throw new Error(`Недопустимое значение R: ${R}. Ожидалось целочисленное значение от 0 до 255.`);
+        } else {
+            return R;
+        }
+    }
+
+    //Создаем константы относительно W
+    generateConstants() {
+        switch (this.W) {
+            case 16:
+                return [
+                    // new Uint8Array([0xB7,0xE1]), 
+                    // new Uint8Array([0x9E,0x37])
+                    "B7E1", "9E37"
+                ]
+            case 32:
+                return [
+                    // new Uint8Array([0xB7,0xE1,0x51,0x63]), 
+                    // new Uint8Array([0x9E, 0x37, 0x79, 0xB9])
+                    "B7E15163", "9E3779B9"
+                ]
+            case 64:
+                return [
+                    // new Uint8Array([0xB7,0xE1,0x51,0x62,0x8A, 0xED, 0x2A, 0x6B]), 
+                    // new Uint8Array([0x9E, 0x37, 0x79, 0xB9, 0x7F, 0x4A, 0x7C, 0x15])
+                    "B7E151628AED2A6B", "9E3779B97F4A7C15"
+                ]
+        }
+    }
+
+    // Создаем массив слов L с длиной c = b/W8
+    keyAlign(){
+         //пустой ключ
+        if (this.b == 0){
+            this.c = 1;
+        }
+        //ключ не кратен w / 8
+        else if (this.b % this.W8 !== 0) {
+            const paddingLength = this.W8 - (this.b % this.W8); // Вычисляем, сколько байтов нужно добавить
+            const padding = new Uint8Array(paddingLength); // Создаём массив байтов 0x00
+            
+            // Создаём новый массив, который объединяет исходный массив и байты 0x00
+            const extendedKey = new Uint8Array(this.Uint8ArrayKey.length + paddingLength);
+            extendedKey.set(this.Uint8ArrayKey); // Копируем исходный массив
+            extendedKey.set(padding, this.Uint8ArrayKey.length); // Добавляем нули
+            
+            this.Uint8ArrayKey = extendedKey;
+            this.b = this.Uint8ArrayKey.length; 
+            this.c = Math.floor(this.b / this.W8);
+        }
+        else{
+            this.c = this.b; // this.W8
+        } 
+        //Задаем кол-во элементов массива L
+        const L = new Array(this.c);
+        //Заполняем массив L
+        for (let i = this.b-1; i >= 0; i--){
+            L[Math.floor(i / this.W8)] = (L[Math.floor(i / this.W8)] << 8) + this.Uint8ArrayKey[i]
+        };
+        this.L = L;
+    }
+
+    extendedKeyTable(){
+        const P = parseInt(this.P, 16);
+        const Q = parseInt(this.Q, 16);
+        const S = new Array(this.T);
+        for (let i = 0; i < this.T; i++){
+            S[i] = (P + i * Q) % this.mod;
+        };
+        this.S = S;
+    }
+
+    mixing() {
+        let i = 0, j = 0, A = 0, B = 0;
+    
+        for (let k = 0; k < (3 * Math.max(this.c, this.T)); k++) {
+            // >>> 0 для беззнакового 32bit числа, хз как это работает *__*
+            A = this.S[i] = this.lshift((this.S[i] + A + B) >>> 0, 3) >>> 0;
+            B = this.L[j] = this.lshift((this.L[j] + A + B) >>> 0, (A + B) >>> 0) >>> 0;
+            i = (i + 1) % this.T;
+            j = (j + 1) % this.c;
+        }
+    
+       
+    }
+    lshift(val, n) {
+        n %= this.W; // Приводим n в диапазон [0, W)
+        return ((val << n) | (val >>> (this.W - n))) >>> 0; // Беззнаковый результат
+    }
+
+    rshift(val, n) {
+        n %= this.W; // Приводим n в диапазон [0, W)
+        return ((val >>> n) | (val << (this.W - n))) & this.mask;
+    }
+
+    encryptString(text){
+        let run = true;
+        let encryptedData = new Uint8Array();
+        let textBytes = new TextEncoder().encode(text);
+        for (let i = 0; i < textBytes.length; i += this.W4) {
+            let block = textBytes.slice(i, i + this.W4);
+            // Проверяем, нужно ли дополнить блок
+            if (block.length < this.W4) {
+                let padding = new Uint8Array(this.W4 - block.length);
+                block = new Uint8Array([...block, ...padding]);  // Дополняем нулевыми байтами
+                run = false;
+            }
+
+            let encryptedBlock = this.encryptBlock(block);
+            encryptedData = new Uint8Array([...encryptedData, ...encryptedBlock]);
+
+            if (!run) {
+                break;
+            }
+        }
+        return encryptedData
+    }
+
+    encryptBlock(data) {
+        // Разбиваем блок на две части A и B
+        let A = this.bytesToInt(data.slice(0, this.W8));
+        let B = this.bytesToInt(data.slice(this.W8));
+
+        // Применяем модификации к A и B
+        A = (A + this.S[0]) % this.mod;
+        B = (B + this.S[1]) % this.mod;
+
+        for (let i = 1; i <= this.R; i++) {
+            A = (this.lshift(A ^ B, B) + this.S[2 * i]) % this.mod;
+            B = (this.lshift(A ^ B, A) + this.S[2 * i + 1]) % this.mod;
+        }
+
+        // Преобразуем A и B обратно в байты и возвращаем зашифрованный блок
+        return this.intToBytes(A, this.W8).concat(this.intToBytes(B, this.W8));
+    }
+
+    // Преобразование массива байтов в целое число
+    bytesToInt(bytes) {
+        return bytes.reduce((acc, byte, index) => acc + (byte << (8 * index)), 0);
+    }
+
+    // Преобразование целого числа в массив байтов
+    intToBytes(value, length) {
+        let bytes = new Array(length);
+        for (let i = 0; i < length; i++) {
+            bytes[i] = (value >> (8 * i)) & 0xFF;
+        }
+        return bytes;
+    }
+
+    decryptString(encryptedData) {
+        let decryptedData = new Uint8Array(); // Для хранения расшифрованных данных
+    
+        for (let i = 0; i < encryptedData.length; i += this.W4) {
+            const block = encryptedData.slice(i, i + this.W4); // Извлекаем текущий блок
+            const decryptedBlock = this.decryptBlock(block);  // Расшифровываем блок
+            decryptedData = new Uint8Array([...decryptedData, ...decryptedBlock]); // Добавляем расшифрованные данные
+        }
+    
+        // Преобразуем байты обратно в строку, удаляя возможные дополнительные нулевые байты
+        return new TextDecoder().decode(decryptedData).replace(/\0+$/, '');
+    }
+    
+    decryptBlock(data) {
+        // Разбиваем блок на две части A и B
+        let A = this.bytesToInt(data.slice(0, this.W8));
+        let B = this.bytesToInt(data.slice(this.W8));
+    
+        // Применяем обратные операции раундов
+        for (let i = this.R; i > 0; i--) {
+            B = this.rshift((B - this.S[2 * i + 1] + this.mod) % this.mod, A) ^ A;
+            A = this.rshift((A - this.S[2 * i] + this.mod) % this.mod, B) ^ B;
+        }
+    
+        // Убираем начальное прибавление
+        B = (B - this.S[1] + this.mod) % this.mod;
+        A = (A - this.S[0] + this.mod) % this.mod;
+    
+        // Преобразование A и B в байты
+        return new Uint8Array([...this.intToBytes(A, this.W8), ...this.intToBytes(B, this.W8)]);
+    }
+    
+}
---
+++ src/app/lab3/page.js
@@ -0,0 +1,127 @@
+"use client"
+
+import "@/styles/labs.scss"
+import "@/styles/basic.scss"
+
+import { useEffect, useRef, useState } from "react"
+import { Lab_3 } from "@/Classes/Lab_3"
+import { useRouter } from "next/navigation"
+import { CustomError } from "@/Classes/CustomError"
+
+export default function Lab3() {
+    const router = useRouter();
+
+    let RC5 = null;
+
+    const refW = useRef(null);
+    const refR = useRef(null);
+    const refKey = useRef(null);
+    const refMessage = useRef(null);
+    const refEncryptMessage = useRef();
+    const refDecryptMessage = useRef(null);
+
+    let encryptedData = null;
+
+    let valueW = 32, valueKey = "keyWord", valueR = 12;
+
+
+    const encryptMessage = () => {
+        try {
+            if (RC5 == null){
+                throw(new CustomError("Ошибка параметров", "Требуется сохранить параметры шифрования!"))
+            } else {
+                let encryptedMessage = RC5.encryptString(refMessage.current.value)
+                encryptedData = encryptedMessage; // для дешифровки
+                let encryptedMessageStr = Array.from(encryptedMessage).map(byte => String.fromCharCode(byte)).join('')
+                refEncryptMessage.current.value = encryptedMessageStr
+            }
+        } catch(e) {
+            handlingError(e)
+        }
+    }
+
+    const decryptMessage = () => {
+        try {
+            if (RC5 == null){
+                throw(new CustomError("Ошибка параметров", "Требуется сохранить параметры шифрования!"))
+            } else {
+                let encryptedMessageArray = new Uint8Array([...refEncryptMessage.current.value].map(char => char.charCodeAt(0)));
+                let decryptedMessage = RC5.decryptString(encryptedData);
+                refDecryptMessage.current.value = decryptedMessage
+            }
+        } catch(e) {
+            handlingError(e)
+        }
+    }
+
+    const saveParams = () => {
+        try {
+            if (refW.current.value != "16" && refW.current.value != "32" && refW.current.value != "64"){
+                throw new CustomError("Ошибка значения W", "W должно быть равно 16, 32 или 64!");
+            } else {
+                valueW = Number(refW.current.value);
+            }
+            if (Number(refR.current.value) < 0 || Number(refR.current.value) > 255){
+                throw new CustomError("Ошибка значения R", "R должно быть в промежутке [0; 255]!");
+            } else {
+                valueR = Number(refR.current.value);
+            }
+            if (refKey.current.value.length > 255) {
+                throw new CustomError("Ошибка ключа", "Ключ должен состоять максимум из 255 символов!");
+            } 
+            else {
+                valueKey = refKey.current.value;
+            }
+            
+            RC5 = new Lab_3({ W: valueW, R: valueR, Key: valueKey });
+            // console.log(RC5)
+        }
+        catch(e){
+            handlingError(e)
+        }
+    }
+
+    const handlingError = (error) => {
+        if (typeof error.alert == "function") {
+            error.alert();
+        } else {
+            console.log(error.message);
+            alert("Произошла непредвиденная ошибка. Подробности в console.log")
+        }
+    }
+
+
+    return (
+        <div className="darkText flex allCenterFlex flexDirrectionColumn">
+            <button className="buttonMainMenu" onClick={() => router.push('/')}>Главное меню</button>
+
+            <div className="block flex allCenterFlex flexDirrectionColumn ">
+                <p>Введите значение W (16, 32, 64)</p>
+                <textarea ref={refW} className="messageTextarea" defaultValue={valueW}></textarea>
+                <p>Введите значение R (0...255)</p>
+                <textarea ref={refR} className="messageTextarea" defaultValue={valueR}></textarea>
+                <p>Введите ключевое слово:</p>
+                <textarea ref={refKey} className="messageTextarea" defaultValue={valueKey}></textarea>
+            </div>
+
+            <button onClick={saveParams} className="standartButton">Сохранить параметры</button>
+
+
+            <div className="block flex allCenterFlex flexDirrectionColumn ">
+                <p>Введите шифруемое сообщение </p>
+                <textarea ref={refMessage} className="messageTextarea"></textarea>
+                <button onClick={encryptMessage} className="standartButton">Зашифровать сообщение</button>
+                <p>Зашифрованное сообщение:</p>
+                <textarea ref={refEncryptMessage} readOnly={true} className="messageTextarea"></textarea>
+            </div>
+
+            <button onClick={decryptMessage} className="standartButton">Расшифровать сообщение</button>
+        
+            <div className="block flex allCenterFlex flexDirrectionColumn ">
+                <p>Расшифрованное сообщение:</p>
+                <textarea ref={refDecryptMessage} readOnly={true} className="messageTextarea"></textarea>
+            </div>
+
+        </div>
+    )
+}
